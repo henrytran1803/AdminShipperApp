ea815e57fef7ce6b56a828a099177ee400c64d2b
Trần Việt Anh <tranvietanh1803@gmail.com>
2024-04-26 15:47:36 +0700

    Initial Commit

---
+++ AdminShipperApp/AdminShipperAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdminShipperAppApp.swift
+//  AdminShipperApp
+//
+//  Created by Tran Viet Anh on 26/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct AdminShipperAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
